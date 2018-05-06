8453ecf4ad2458f5ac2697f503ea792dd003996d
Stepan Kalinin <kalininstep@mail.ru>
2018-05-06 15:40:50 +0300

    6-lock-free/stack task solution changed

--- 6-lock-free/stack/solution.hpp
+++ 6-lock-free/stack/solution.hpp
@@ -2,19 +2,20 @@
 
 #include <tpcc/stdlike/atomic.hpp>
 #include <tpcc/support/compiler.hpp>
+#include <tpcc/lockfree/x86_64_atomic_stamped_pointer.hpp>
 
 #include <utility>
 
 namespace tpcc {
 namespace solutions {
 
-// Treiber lock-free stack
+using namespace lockfree;
 
 template <typename T>
 class LockFreeStack {
   struct Node {
     T item_;
-    // to be continued
+    tpcc::atomic<Node*> next_;
 
     Node(T item) : item_(std::move(item)) {
     }
@@ -25,21 +26,63 @@ class LockFreeStack {
   }
 
   ~LockFreeStack() {
-    // not implemented
+    CleanStack(top_);
+    CleanStack(pool_top_);
   }
 
   void Push(T item) {
-    UNUSED(item);
-    // not implemented
+    Node* node = CreateNode(std::move(item));
+    AttachNode(node, top_);
   }
 
   bool Pop(T& item) {
-    UNUSED(item);
-    return false;  // not implemented
+    Node* node = DetachNode(top_);
+    if (!node)
+      return false;
+    item = std::move(node->item_);
+    AttachNode(node, pool_top_);
+    return true;
   }
 
  private:
-  tpcc::atomic<Node*> top_{nullptr};
+  using TopPtr = X86AtomicStampedPointer<Node>;
+
+  void AttachNode(Node* node, TopPtr& top) {
+    auto curr_top = top_.Load();
+    do {
+      node->next_ = curr_top.raw_ptr_;
+    } while (!top.CompareAndSet(curr_top, node));
+  }
+
+  Node* DetachNode(TopPtr& top) {
+    auto curr_top = top.Load();
+    do {
+      if (!curr_top) {
+        return nullptr;
+      }
+    } while (!top.CompareAndSet(curr_top, curr_top->next_));
+    return curr_top.raw_ptr_;
+  }
+
+  Node* CreateNode(T item) {
+    Node* node = DetachNode(pool_top_);
+    if (!node) {
+      return new Node(std::move(item));
+    }
+    return node;
+  }
+
+  void CleanStack(TopPtr& top) {
+    Node* node = top.LoadPointer();
+    while (node) {
+      Node* next = node->next_;
+      delete node;
+      node = next;
+    }
+  }
+
+  TopPtr top_{nullptr, 0};
+  TopPtr pool_top_{nullptr, 0};
 };
 
 }  // namespace solutions
